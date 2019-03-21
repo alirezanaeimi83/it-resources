3ae229cf8b9dd561f45bdf1e496c8b692c25909e
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-03-20 22:11:26 -0700

    Update Windows-10-Deboat.bat

--- Scripts/Windows/Windows-10-Deboat.bat
+++ Scripts/Windows/Windows-10-Deboat.bat
@@ -1,4 +1,7 @@
 REM Install basic software for new Windows setup
 REM Author: Justin Chapdelaine
 REM Version: 20190320.1
-REM Source: https://github.com/justinchapdelaine/latteit
+REM Source: https://github.com/justinchapdelaine/IT-Resources
+
+REM Remove global Windows 10 Apps
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "get-appxpackage *officehub* | remove-appxpackage"
