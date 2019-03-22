7b7e3694f6437b3d51ead38905fd6e1dd109988a
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-03-21 23:55:12 -0700

    Create Restore-PowerPlan-Defaults.bat

---
+++ Scripts/Windows/Restore-PowerPlan-Defaults.bat
@@ -0,0 +1,8 @@
+REM Restore PowerPlan to defaults
+REM Author: Justin Chapdelaine
+REM Version: 20190321.1
+REM Source: https://github.com/justinchapdelaine/IT-Resources
+
+powercfg -restoredefaultschemes
+
+PAUSE
