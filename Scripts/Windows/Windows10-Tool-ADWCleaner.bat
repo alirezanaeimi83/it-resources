89cf579886c769b26ba3e429bce2eb7573e411f0
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-03-23 14:33:01 -0700

    bugfix: Change download directory

--- Scripts/Windows/Windows10-Tool-ADWCleaner.bat
+++ Scripts/Windows/Windows10-Tool-ADWCleaner.bat
@@ -4,7 +4,7 @@ REM Version: 20190323.1
 REM Source: https://github.com/justinchapdelaine/IT-Resources
 
 REM Downloading ADWCleaner to C:\STS
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "& {Invoke-WebRequest 'https://download.bleepingcomputer.com/dl/8f2cfe815fa12a8f7a057a0dbc9f879a/5c96a4c6/windows/security/security-utilities/a/adwcleaner/AdwCleaner.exe'; Start-Process -FilePath C:\STS -Verb RunAs -Wait}"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "& {Invoke-WebRequest -Uri 'https://download.bleepingcomputer.com/dl/8f2cfe815fa12a8f7a057a0dbc9f879a/5c96a4c6/windows/security/security-utilities/a/adwcleaner/AdwCleaner.exe' -OutFile 'C:\STS'}"
 
 REM Download Done!
 PAUSE
