a58aa5a88c39c2347d15219c0218edeb65fd654b
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-03-23 14:51:14 -0700

    bugfix: Changed download url

--- Scripts/Windows/Windows10-Tool-ADWCleaner.bat
+++ Scripts/Windows/Windows10-Tool-ADWCleaner.bat
@@ -5,7 +5,7 @@ REM Source: https://github.com/justinchapdelaine/IT-Resources
 
 REM Downloading ADWCleaner to C:\STS\
 md C:\STS
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "& {Invoke-WebRequest -Uri 'https://www.fosshub.com/Malwarebytes-AdwCleaner.html?dwl=adwcleaner_7.2.7.0.exe' -OutFile 'C:\STS\ADWCleaner.exe'}"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "& {Invoke-WebRequest -Uri 'http://files2.majorgeeks.com/4ce393c4a7caa63badce4afca5a905c786a66d4f/spyware/adwcleaner-7.2.7.0.exe' -OutFile 'C:\STS\ADWCleaner.exe'}"
 
 REM Download Done!
 PAUSE
