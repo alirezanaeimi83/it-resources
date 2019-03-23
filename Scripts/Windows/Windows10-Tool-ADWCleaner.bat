203a0f3af2186a7ce264301998724895b272034e
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-03-23 14:44:54 -0700

    bugfix: Need to create STS directory first

--- Scripts/Windows/Windows10-Tool-ADWCleaner.bat
+++ Scripts/Windows/Windows10-Tool-ADWCleaner.bat
@@ -4,6 +4,7 @@ REM Version: 20190323.1
 REM Source: https://github.com/justinchapdelaine/IT-Resources
 
 REM Downloading ADWCleaner to C:\STS\
+md C:\STS
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "& {Invoke-WebRequest -Uri 'https://www.fosshub.com/Malwarebytes-AdwCleaner.html?dwl=adwcleaner_7.2.7.0.exe' -OutFile 'C:\STS\ADWCleaner.exe'}"
 
 REM Download Done!
