38a2357451ed1bf41931b7d7ac6d60b0332a5ac6
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-03-23 13:28:37 -0700

    Create Windows10-Install-Chrome.bat

---
+++ Scripts/Windows/Windows10-Install-Chrome.bat
@@ -0,0 +1,10 @@
+REM Install Google Chrome
+REM Author: Justin Chapdelaine
+REM Version: 20190323.1
+REM Source: https://github.com/justinchapdelaine/IT-Resources
+
+REM Installing Google Chrome version Latest
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "& {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/silent /install' -Verb RunAs -Wait; Remove-Item $P}"
+
+REM Installation Done!
+PAUSE
