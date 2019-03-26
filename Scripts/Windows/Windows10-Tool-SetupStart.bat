98d6d946b2fd959c0371b063dd26186273b0aab7
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-03-25 23:10:57 -0700

    style: Clearer restore point name

--- Scripts/Windows/Windows10-Tool-SetupStart.bat
+++ Scripts/Windows/Windows10-Tool-SetupStart.bat
@@ -12,6 +12,6 @@ REM S.M.A.R.T. status
 wmic diskdrive get status
 
 REM Create initial restore point
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Checkpoint-Computer -Description 'STAPLES - Start Setup'"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Checkpoint-Computer -Description 'STAPLES - Setup Start'"
 
 PAUSE
