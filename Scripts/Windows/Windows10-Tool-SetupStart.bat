ec91928984915a17fd07ec379889ee4fe4a24843
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-03-28 01:58:59 -0700

    feat: Disable sleep and monitor timeout
    Also cleaner messaging

--- Scripts/Windows/Windows10-Tool-SetupStart.bat
+++ Scripts/Windows/Windows10-Tool-SetupStart.bat
@@ -1,17 +1,26 @@
+@echo off
+
 REM Start Service will get serial number, disable sleep, and get S.M.A.R.T. status
 REM Author: Justin Chapdelaine
 REM Version: 20190323.1
 REM Source: https://github.com/justinchapdelaine/IT-Resources
 
-REM Serial Number
+echo Serial Number
 wmic bios get serialnumber
+rem
 
-REM Disable sleep
+echo Disable sleep and monitor timeout
+powercfg /change monitor-timeout-ac 0
+powercfg /change standby-timeout-ac 0
+rem
 
-REM S.M.A.R.T. status
+echo S.M.A.R.T. status
 wmic diskdrive get status
+rem
 
 REM Create initial restore point
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Checkpoint-Computer -Description 'STAPLES - Setup Start'"
+rem
 
+echo Start your setup!
 PAUSE
