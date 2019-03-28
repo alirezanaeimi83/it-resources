dca8aabfe01ac692d9de894590fb89a6d21567ff
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-03-28 02:02:05 -0700

    feat: Admin check

--- Scripts/Windows/Windows10-Tool-SetupStart.bat
+++ Scripts/Windows/Windows10-Tool-SetupStart.bat
@@ -1,26 +1,51 @@
 @echo off
 
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
 REM Start Service will get serial number, disable sleep, and get S.M.A.R.T. status
 REM Author: Justin Chapdelaine
 REM Version: 20190323.1
 REM Source: https://github.com/justinchapdelaine/IT-Resources
 
-echo Serial Number
+rem Serial Number
 wmic bios get serialnumber
-rem
+rem 
 
 echo Disable sleep and monitor timeout
 powercfg /change monitor-timeout-ac 0
 powercfg /change standby-timeout-ac 0
-rem
+rem 
 
 echo S.M.A.R.T. status
 wmic diskdrive get status
-rem
+rem 
 
 REM Create initial restore point
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Checkpoint-Computer -Description 'STAPLES - Setup Start'"
-rem
+rem 
 
 echo Start your setup!
 PAUSE
