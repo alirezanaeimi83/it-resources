60d09fbe464eaeb2641034fd32553571b5948ab3
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-03-31 07:13:19 -0700

    fix: Removed argument flag
    Empty argument flag was preventing running the program

--- Scripts/Windows/Windows10-Tool-ADWCleaner.bat
+++ Scripts/Windows/Windows10-Tool-ADWCleaner.bat
@@ -6,7 +6,7 @@ REM Version: 20190330.1
 REM Source: https://github.com/justinchapdelaine/IT-Resources
 
 echo Downloading ADWCleaner
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "& {$P = $env:TEMP + '\adwcleaner_7.2.7.0.exe'; Invoke-WebRequest 'https://dl.dropboxusercontent.com/s/y11kuufqjgbvgh7/adwcleaner_7.2.7.0.exe' -OutFile $P; Start-Process -FilePath $P -Args '' -Verb RunAs -Wait; Remove-Item $P}"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "& {$P = $env:TEMP + '\adwcleaner_7.2.7.0.exe'; Invoke-WebRequest 'https://dl.dropboxusercontent.com/s/y11kuufqjgbvgh7/adwcleaner_7.2.7.0.exe' -OutFile $P; Start-Process -FilePath $P -Verb RunAs -Wait; Remove-Item $P}"
 echo:
 
 PAUSE
