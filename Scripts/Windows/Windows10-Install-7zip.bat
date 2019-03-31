6cfa320916e9c30a775fb26345425126d6cc287f
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-03-31 06:17:03 -0700

    doc: Output clarification

--- Scripts/Windows/Windows10-Install-7zip.bat
+++ Scripts/Windows/Windows10-Install-7zip.bat
@@ -1,10 +1,13 @@
+@echo off
+
 REM Install 7-zip
 REM Author: Justin Chapdelaine
 REM Version: 20190323.1
 REM Source: https://github.com/justinchapdelaine/IT-Resources
 
-REM Installing 7-zip version 19.00
+echo Installing 7-zip version 19.00
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "& {$P = $env:TEMP + '\7z1900-x64.exe'; Invoke-WebRequest 'https://www.7-zip.org/a/7z1900-x64.exe' -OutFile $P; Start-Process -FilePath $P -Args '/S /D="%PROGRAMFILES%\7-Zip"' -Verb RunAs -Wait; Remove-Item $P}"
+echo:
 
-REM Installation Done!
+echo Installation Done!
 PAUSE
