b40d92ea68488c9c2b19e23bc3a91a8723ddbbf0
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-03-31 06:12:51 -0700

    fix: Changed %20 for spaces in url

--- Scripts/Windows/Windows10-Install-Firefox.bat
+++ Scripts/Windows/Windows10-Install-Firefox.bat
@@ -6,7 +6,7 @@ REM Version: 20190330.1
 REM Source: https://github.com/justinchapdelaine/IT-Resources
 
 echo Installing Mozilla Firefox version 66.0.2
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "& {$P = $env:TEMP + '\Firefox%20Setup%2066.0.2.exe'; Invoke-WebRequest 'https://ftp.mozilla.org/pub/firefox/releases/66.0.2/win64/en-CA/Firefox%20Setup%2066.0.2.exe' -OutFile $P; Start-Process -FilePath $P -Args '-ms' -Verb RunAs -Wait; Remove-Item $P}"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "& {$P = $env:TEMP + '\Firefox Setup 66.0.2.exe'; Invoke-WebRequest 'https://ftp.mozilla.org/pub/firefox/releases/66.0.2/win64/en-CA/Firefox Setup 66.0.2.exe' -OutFile $P; Start-Process -FilePath $P -Args '-ms' -Verb RunAs -Wait; Remove-Item $P}"
 echo:
 
 echo Installation Done!
