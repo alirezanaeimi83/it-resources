c2570f929cecdc53c8b8ff27e3f4dac677dd79fa
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-03-31 06:18:44 -0700

    doc: Output clarification

--- Scripts/Windows/Windows10-Install-AdobeReader.bat
+++ Scripts/Windows/Windows10-Install-AdobeReader.bat
@@ -1,10 +1,13 @@
+@echo off
+
 REM Install Adobe Reader DC
 REM Author: Justin Chapdelaine
-REM Version: 20190323.1
+REM Version: 20190330.1
 REM Source: https://github.com/justinchapdelaine/IT-Resources
 
-REM Installing Adobe Reader DC version 2019.010.20098
+echo Installing Adobe Reader DC version 2019.010.20098
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "& {$P = $env:TEMP + '\AcroRdrDC1901020098_en_US.exe'; Invoke-WebRequest 'ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1901020098/AcroRdrDC1901020098_en_US.exe' -OutFile $P; Start-Process -FilePath $P -Args '/sAll /rs' -Verb RunAs -Wait; Remove-Item $P}"
+echo:
 
-REM Installation Done!
+echo Installation Done!
 PAUSE
