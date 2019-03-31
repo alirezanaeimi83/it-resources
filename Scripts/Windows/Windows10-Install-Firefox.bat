0245404b7bd933ef0f3c1ddf05e451a8d39e9498
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-03-31 06:05:28 -0700

    fix: File name was incorrect

--- Scripts/Windows/Windows10-Install-Firefox.bat
+++ Scripts/Windows/Windows10-Install-Firefox.bat
@@ -1,12 +1,12 @@
 @echo off
 
-REM Install Google Chrome
+REM Install Mozilla Firefox
 REM Author: Justin Chapdelaine
 REM Version: 20190330.1
 REM Source: https://github.com/justinchapdelaine/IT-Resources
 
 echo Installing Mozilla Firefox version 66.0.2
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "& {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://download-installer.cdn.mozilla.net/pub/firefox/releases/66.0.2/win64/en-CA/Firefox%20Setup%2066.0.2.exe' -OutFile $P; Start-Process -FilePath $P -Args '-ms' -Verb RunAs -Wait; Remove-Item $P}"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "& {$P = $env:TEMP + '\Firefox%20Setup%2066.0.2.exe'; Invoke-WebRequest 'https://download-installer.cdn.mozilla.net/pub/firefox/releases/66.0.2/win64/en-CA/Firefox%20Setup%2066.0.2.exe' -OutFile $P; Start-Process -FilePath $P -Args '-ms' -Verb RunAs -Wait; Remove-Item $P}"
 echo:
 
 echo Installation Done!
