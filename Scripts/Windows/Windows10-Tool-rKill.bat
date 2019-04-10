2cce303995e116faaddbc065abcff0dbe59c04e7
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-04-10 02:56:04 -0700

    docs: Clarity on output

--- Scripts/Windows/Windows10-Tool-rKill.bat
+++ Scripts/Windows/Windows10-Tool-rKill.bat
@@ -2,14 +2,16 @@
 
 REM Download and run rKill
 REM Author: Justin Chapdelaine
-REM Version: 20190409.2
+REM Version: 20190409.3
 REM Source: https://github.com/justinchapdelaine/IT-Resources
 
 echo Downloading rKill
 bitsadmin /transfer "Downloading rKill..." /download /priority normal https://github.com/justinchapdelaine/IT-Resources/raw/master/Tools/rk.exe C:\STS\Programs\rk.exe
+echo:
 
 echo Start rKill
 start C:\STS\Programs\rk.exe
+echo:
 
 PAUSE
 
