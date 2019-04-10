7ecb995fc0d5efd8cbd1ef773a5b11e9ab1a39b4
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-04-10 03:12:24 -0700

    bugfix: Start program name fixed

--- Scripts/Windows/Windows10-Tool-rKill.bat
+++ Scripts/Windows/Windows10-Tool-rKill.bat
@@ -5,12 +5,15 @@ REM Author: Justin Chapdelaine
 REM Version: 20190409.3
 REM Source: https://github.com/justinchapdelaine/IT-Resources
 
+echo Create STS directory
+mkdir C:\STS\Programs
+
 echo Downloading rKill 2.9.1.0
 bitsadmin /transfer "Downloading rKill..." /download /priority normal https://dl.dropboxusercontent.com/s/7n94ql9zog1l1uf/rkill.exe C:\STS\Programs\rkill.exe
 echo:
 
 echo Start rKill
-start C:\STS\Programs\rk.exe
+start C:\STS\Programs\rkill.exe
 echo:
 
 PAUSE
