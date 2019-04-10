4edbdd5bc32540323c1e7c0c6c25c9ad21a40599
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-04-10 03:10:21 -0700

    feat: New download source

--- Scripts/Windows/Windows10-Tool-rKill.bat
+++ Scripts/Windows/Windows10-Tool-rKill.bat
@@ -5,8 +5,8 @@ REM Author: Justin Chapdelaine
 REM Version: 20190409.3
 REM Source: https://github.com/justinchapdelaine/IT-Resources
 
-echo Downloading rKill
-bitsadmin /transfer "Downloading rKill..." /download /priority normal https://github.com/justinchapdelaine/IT-Resources/raw/master/Tools/rk.exe C:\STS\Programs\rk.exe
+echo Downloading rKill 2.9.1.0
+bitsadmin /transfer "Downloading rKill..." /download /priority normal https://dl.dropboxusercontent.com/s/7n94ql9zog1l1uf/rkill.exe C:\STS\Programs\rkill.exe
 echo:
 
 echo Start rKill
@@ -14,4 +14,3 @@ start C:\STS\Programs\rk.exe
 echo:
 
 PAUSE
-
