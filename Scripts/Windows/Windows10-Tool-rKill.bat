43fb843f0aae9b0921fd731e28d8f88889de3b28
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-04-10 02:30:28 -0700

    feat: Rebuilt to be usable on Windows 7 and 8

--- Scripts/Windows/Windows10-Tool-rKill.bat
+++ Scripts/Windows/Windows10-Tool-rKill.bat
@@ -2,11 +2,13 @@
 
 REM Download and run rKill
 REM Author: Justin Chapdelaine
-REM Version: 20190401.1
+REM Version: 20190409.2
 REM Source: https://github.com/justinchapdelaine/IT-Resources
 
 echo Downloading rKill
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "& {$P = $env:TEMP + '\rk.exe'; Invoke-WebRequest 'https://github.com/justinchapdelaine/IT-Resources/raw/master/Tools/rk.exe' -OutFile $P; Start-Process -FilePath $P -Verb RunAs -Wait; Remove-Item $P}"
-echo:
+bitsadmin /transfer "Downloading rKill..." /download /priority normal https://github.com/justinchapdelaine/IT-Resources/raw/master/Tools/rk.exe C:\STS\Programs\rk.exe
+
+start C:\STS\Programs\rk.exe
 
 PAUSE
+
