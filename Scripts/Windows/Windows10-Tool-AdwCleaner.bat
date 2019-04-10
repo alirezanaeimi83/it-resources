a80602f5137940ec739fdf6434b712313823ec7f
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-04-10 02:55:41 -0700

    docs: Clarity on output

--- Scripts/Windows/Windows10-Tool-AdwCleaner.bat
+++ Scripts/Windows/Windows10-Tool-AdwCleaner.bat
@@ -7,8 +7,10 @@ REM Source: https://github.com/justinchapdelaine/IT-Resources
 
 echo Downloading AdwCleaner
 bitsadmin /transfer "Downloading AdwCleaner..." /download /priority normal https://github.com/justinchapdelaine/IT-Resources/raw/master/Tools/adw.exe C:\STS\Programs\adw.exe
+echo:
 
 echo Start AdwCleaner
 start C:\STS\Programs\adw.exe
+echo:
 
 PAUSE
