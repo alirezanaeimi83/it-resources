502feb913c4a5afbfb30466b5001869f09402aa9
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-04-10 02:50:18 -0700

    Update Windows10-Tool-rKill.bat

--- Scripts/Windows/Windows10-Tool-rKill.bat
+++ Scripts/Windows/Windows10-Tool-rKill.bat
@@ -8,6 +8,7 @@ REM Source: https://github.com/justinchapdelaine/IT-Resources
 echo Downloading rKill
 bitsadmin /transfer "Downloading rKill..." /download /priority normal https://github.com/justinchapdelaine/IT-Resources/raw/master/Tools/rk.exe C:\STS\Programs\rk.exe
 
+echo Start rKill
 start C:\STS\Programs\rk.exe
 
 PAUSE
