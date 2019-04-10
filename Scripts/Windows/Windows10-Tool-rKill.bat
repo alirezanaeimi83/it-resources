663559683f9f5931f1b08158e78e3543ec2a5b6b
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-04-10 03:13:12 -0700

    docs: Clarity on output

--- Scripts/Windows/Windows10-Tool-rKill.bat
+++ Scripts/Windows/Windows10-Tool-rKill.bat
@@ -5,14 +5,15 @@ REM Author: Justin Chapdelaine
 REM Version: 20190409.3
 REM Source: https://github.com/justinchapdelaine/IT-Resources
 
-echo Create STS directory
+echo 1. Create STS directory
 mkdir C:\STS\Programs
+echo:
 
-echo Downloading rKill 2.9.1.0
+echo 2. Downloading rKill 2.9.1.0
 bitsadmin /transfer "Downloading rKill..." /download /priority normal https://dl.dropboxusercontent.com/s/7n94ql9zog1l1uf/rkill.exe C:\STS\Programs\rkill.exe
 echo:
 
-echo Start rKill
+echo 3. Start rKill
 start C:\STS\Programs\rkill.exe
 echo:
 
