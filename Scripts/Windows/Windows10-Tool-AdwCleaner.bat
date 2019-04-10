de280bcc83f5ab728625d47d631f9e784c735207
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-04-10 03:13:51 -0700

    bugfix: Fixed start program name

--- Scripts/Windows/Windows10-Tool-AdwCleaner.bat
+++ Scripts/Windows/Windows10-Tool-AdwCleaner.bat
@@ -5,12 +5,16 @@ REM Author: Justin Chapdelaine
 REM Version: 20190409.3
 REM Source: https://github.com/justinchapdelaine/IT-Resources
 
+echo 1. Create STS directory
+mkdir C:\STS\Programs
+echo:
+
 echo Downloading AdwCleaner 7.3
 bitsadmin /transfer "Downloading AdwCleaner..." /download /priority normal https://dl.dropboxusercontent.com/s/h8rwawulb1i64l8/adwcleaner_7.3.exe C:\STS\Programs\adwcleaner_7.3.exe
 echo:
 
 echo Start AdwCleaner
-start C:\STS\Programs\adw.exe
+start C:\STS\Programs\adwcleaner_7.3.exe
 echo:
 
 PAUSE
