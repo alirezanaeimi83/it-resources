186e17a18b047f1fac769beb4b1de2a031273c28
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-04-10 03:08:38 -0700

    bugfix: New download source

--- Scripts/Windows/Windows10-Tool-AdwCleaner.bat
+++ Scripts/Windows/Windows10-Tool-AdwCleaner.bat
@@ -2,11 +2,11 @@
 
 REM Download and run AdwCleaner
 REM Author: Justin Chapdelaine
-REM Version: 20190409.2
+REM Version: 20190409.3
 REM Source: https://github.com/justinchapdelaine/IT-Resources
 
-echo Downloading AdwCleaner
-bitsadmin /transfer "Downloading AdwCleaner..." /download /priority normal https://github.com/justinchapdelaine/IT-Resources/raw/master/Tools/adw.exe C:\STS\Programs\adw.exe
+echo Downloading AdwCleaner 7.3
+bitsadmin /transfer "Downloading AdwCleaner..." /download /priority normal https://dl.dropboxusercontent.com/s/h8rwawulb1i64l8/adwcleaner_7.3.exe C:\STS\Programs\adwcleaner_7.3.exe
 echo:
 
 echo Start AdwCleaner
