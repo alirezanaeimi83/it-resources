5366ded540532084b92e309ac32740632fe841d7
justinchapdelaine <justinchapdelaine+programming@gmail.com>
2019-04-11 22:28:02 -0700

    feat: New version of Adobe Reader DC

--- Scripts/Windows/Installer/Install-AdobeReader.bat
+++ Scripts/Windows/Installer/Install-AdobeReader.bat
@@ -5,8 +5,8 @@ REM Author: Justin Chapdelaine
 REM Version: 20190330.1
 REM Source: https://github.com/justinchapdelaine/IT-Resources
 
-echo Installing Adobe Reader DC version 2019.010.20098
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "& {$P = $env:TEMP + '\AcroRdrDC1901020098_en_US.exe'; Invoke-WebRequest 'ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1901020098/AcroRdrDC1901020098_en_US.exe' -OutFile $P; Start-Process -FilePath $P -Args '/sAll /rs' -Verb RunAs -Wait; Remove-Item $P}"
+echo Installing Adobe Reader DC version 2019.010.20099
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "& {$P = $env:TEMP + '\AcroRdrDC1901020099_en_US.exe'; Invoke-WebRequest 'ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1901020099/AcroRdrDC1901020099_en_US.exe' -OutFile $P; Start-Process -FilePath $P -Args '/sAll /rs' -Verb RunAs -Wait; Remove-Item $P}"
 echo:
 
 echo Installation Done!
